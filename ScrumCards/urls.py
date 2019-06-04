8f8b7976ecfec2f9932fa8cad62d9bcb262ace91
Kirya.conf <kirya.conf@mail.ru>
2019-06-04 07:24:25 +0700

    minor changes to auth_api

--- ScrumCards/urls.py
+++ ScrumCards/urls.py
@@ -23,7 +23,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('scrumboard/', include('scrumboard.urls')),
-    path('', TemplateView.as_view(template_name="scrumboard/home.html"))
+    path('', TemplateView.as_view(template_name="scrumboard/home.html")),
+    path('auth_api/', include('auth_api.urls'))
 ]
 
 if settings.DEBUG:
--- auth_api/api.py
+++ auth_api/api.py
@@ -1,5 +1,29 @@
-from rest_framework import views
+from rest_framework import views, status
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
 
 class LoginView(views.APIView):
 
-    def
+    def post(self, request):
+        user = authenticate(
+            username = request.data.get("username"),
+            password = request.data.get("password"))
+
+        if user is None or not user.is_active:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username or password incorrect'
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+        login(request, user)
+
+        return Response(UserSerializer(user).data)
+
+
+class LogoutView(views.APIView):
+
+    def get(self, request):
+        logout(request)
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
